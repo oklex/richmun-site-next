10937ec31c8eff0e47c6a82679dacfa6d9a378f3
Alexander Kim <alexander@refractionpoint.com>
2023-12-10 17:10:46 -0800

    added prototype contact page

---
+++ src/pages/contact.tsx
@@ -0,0 +1,11 @@
+import React, { ReactElement } from "react"
+
+const Contact = (): ReactElement => {
+    return (
+        <div>
+            Hello world
+        </div>
+    )
+}
+
+export default Contact
