a804a4f6231e84041d0224b50177c5916c3c1833
Alexander Kim <alexander@refractionpoint.com>
2023-12-16 17:26:33 -0800

    updated export name

--- src/lib/conferenceMeta/index.ts
+++ src/lib/conferenceMeta/index.ts
@@ -1,4 +1,4 @@
 export { ITERATION_EIGHT } from './i8';
-export { ITERATION_NINE, ITERATION_NINE as ConferenceMeta } from './i9';
+export { ITERATION_NINE, ITERATION_NINE as CONFERENCE_METADATA } from './i9';
 
 // set the latest meta to be the main export as 'ConferenceMeta'
